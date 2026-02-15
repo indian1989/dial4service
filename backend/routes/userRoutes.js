feda47b85981615be1a5b21a5e8fbc6cf5f16cc4
indian1989 <rahmathussain.hjp@gmail.com>
2026-02-15 20:02:17 +0530

    Implement user registration and login routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,1 +1,21 @@
-userRoutes.js
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+router.post("/register", async(req,res)=>{
+  const hash = await bcrypt.hash(req.body.password,10);
+  await new User({...req.body,password:hash}).save();
+  res.json("User Registered");
+});
+
+router.post("/login", async(req,res)=>{
+  const user = await User.findOne({email:req.body.email});
+  const ok = await bcrypt.compare(req.body.password,user.password);
+  if(!ok) return res.sendStatus(400);
+
+  const token = jwt.sign({id:user._id,role:"user"},process.env.JWT_SECRET);
+  res.json({token});
+});
+
+module.exports = router;
