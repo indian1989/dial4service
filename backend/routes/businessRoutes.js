1767151f83049bd4d317cd527bfb55b120e62bf9
indian1989 <rahmathussain.hjp@gmail.com>
2026-02-15 20:16:03 +0530

    Update businessRoutes.js

--- backend/routes/businessRoutes.js
+++ backend/routes/businessRoutes.js
@@ -13,3 +13,12 @@ router.get("/approved", async(req,res)=>{
 });
 
 module.exports = router;
+router.get("/search", async(req,res)=>{
+  const { city, category } = req.query;
+  const data = await Business.find({
+    city,
+    category,
+    status: "approved"
+  });
+  res.json(data);
+});
