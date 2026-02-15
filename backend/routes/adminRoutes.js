cd7be6d836d830bae9228391a1c4c660e43c80de
indian1989 <rahmathussain.hjp@gmail.com>
2026-02-15 20:04:02 +0530

    Update adminRoutes.js

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,1 +1,15 @@
-adminRoutes.js
+const router = require("express").Router();
+const Admin = require("../models/Admin");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+router.post("/login", async(req,res)=>{
+  const a = await Admin.findOne({email:req.body.email});
+  const ok = await bcrypt.compare(req.body.password,a.password);
+  if(!ok) return res.sendStatus(400);
+
+  const token = jwt.sign({id:a._id,role:"admin"},process.env.JWT_SECRET);
+  res.json({token});
+});
+
+module.exports = router;
