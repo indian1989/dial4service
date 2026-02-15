5b5b3205e71470f5b4a560e15eb30be95397f291
indian1989 <rahmathussain.hjp@gmail.com>
2026-02-15 19:58:45 +0530

    Create Admin model with email, password, and role

--- backend/models/Admin.js
+++ backend/models/Admin.js
@@ -1,1 +1,9 @@
-Admin.js
+const mongoose = require("mongoose");
+
+module.exports = mongoose.model("Admin",
+  new mongoose.Schema({
+    email: String,
+    password: String,
+    role: { type: String, default: "admin" }
+  })
+);
