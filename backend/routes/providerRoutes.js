f7dcbb25c8a983892f30b994ed6857362d3fa670
indian1989 <rahmathussain.hjp@gmail.com>
2026-02-15 20:03:13 +0530

    Update providerRoutes.js

--- backend/routes/providerRoutes.js
+++ backend/routes/providerRoutes.js
@@ -1,1 +1,21 @@
-providerRoutes.js
+const router = require("express").Router();
+const Provider = require("../models/Provider");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+router.post("/register", async(req,res)=>{
+  const hash = await bcrypt.hash(req.body.password,10);
+  await new Provider({...req.body,password:hash}).save();
+  res.json("Provider Registered");
+});
+
+router.post("/login", async(req,res)=>{
+  const p = await Provider.findOne({email:req.body.email});
+  const ok = await bcrypt.compare(req.body.password,p.password);
+  if(!ok) return res.sendStatus(400);
+
+  const token = jwt.sign({id:p._id,role:"provider"},process.env.JWT_SECRET);
+  res.json({token});
+});
+
+module.exports = router;
