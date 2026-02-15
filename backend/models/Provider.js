ee5161d4d1991517350fba5fe6b6ad173382b351
indian1989 <rahmathussain.hjp@gmail.com>
2026-02-15 19:58:07 +0530

    Create Provider model with schema definition

--- backend/models/Provider.js
+++ backend/models/Provider.js
@@ -1,1 +1,10 @@
-Provider.js
+const mongoose = require("mongoose");
+
+module.exports = mongoose.model("Provider",
+  new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,
+    role: { type: String, default: "provider" }
+  })
+);
