bac8e2700d69668cbc99341b6e47b6435fae254f
indian1989 <rahmathussain.hjp@gmail.com>
2026-02-15 20:05:32 +0530

    Update businessRoutes.js

--- backend/routes/businessRoutes.js
+++ backend/routes/businessRoutes.js
@@ -1,1 +1,15 @@
-businessRoutes.js
+const router = require("express").Router();
+const Business = require("../models/Business");
+const auth = require("../middleware/auth");
+
+router.post("/add", auth("provider"), async(req,res)=>{
+  await new Business({...req.body,providerId:req.user.id}).save();
+  res.json("Business Added (Pending)");
+});
+
+router.get("/approved", async(req,res)=>{
+  const data = await Business.find({status:"approved"});
+  res.json(data);
+});
+
+module.exports = router;
