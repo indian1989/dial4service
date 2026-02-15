9615b2dee6279e623dd51fec04830cc827456023
indian1989 <rahmathussain.hjp@gmail.com>
2026-02-15 20:01:26 +0530

    Add JWT authentication middleware
    Implement JWT authentication middleware with role check.

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,1 +1,12 @@
-auth.js
+const jwt = require("jsonwebtoken");
+
+module.exports = (role) => (req,res,next)=>{
+  const token = req.headers.authorization;
+  if(!token) return res.sendStatus(401);
+
+  const data = jwt.verify(token, process.env.JWT_SECRET);
+  if(role && data.role !== role) return res.sendStatus(403);
+
+  req.user = data;
+  next();
+};
