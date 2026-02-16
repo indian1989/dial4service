ab49b4f4af25ed9baf495b00a3876f66fe295ae1
indian1989 <rahmathussain.hjp@gmail.com>
2026-02-16 15:28:29 +0530

    Update provider.js

--- frontend/js/provider.js
+++ frontend/js/provider.js
@@ -1,1 +1,14 @@
-provider.js
+function addBusiness(){
+ fetch(`${API_BASE_URL}/api/business/add`,{
+  method:"POST",
+  headers:{
+   "Content-Type":"application/json",
+   "Authorization":"Bearer "+localStorage.getItem("token")
+  },
+  body:JSON.stringify({
+   title:"AC Repair",
+   city:"Delhi",
+   category:"Home"
+  })
+ });
+}
