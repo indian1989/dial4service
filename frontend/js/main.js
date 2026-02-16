27a09d3059dff66f3ebbf5a566345c36d2ecf9dc
indian1989 <rahmathussain.hjp@gmail.com>
2026-02-16 15:23:35 +0530

    Update main.js

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,1 +1,9 @@
-main.js
+fetch(`${API_BASE_URL}/api/business`)
+.then(res=>res.json())
+.then(data=>{
+ let html="";
+ data.forEach(b=>{
+  html+=`<p>${b.title} - ${b.city}</p>`;
+ });
+ document.getElementById("list").innerHTML=html;
+});
