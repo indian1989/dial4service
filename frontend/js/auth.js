7f03a6b633d375af65d4a8f18f5d07b79fbbfea0
indian1989 <rahmathussain.hjp@gmail.com>
2026-02-16 15:25:32 +0530

    Update auth.js

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -1,1 +1,14 @@
-User.js
+function login(){
+ fetch(`${API_BASE_URL}/api/users/login`,{
+  method:"POST",
+  headers:{"Content-Type":"application/json"},
+  body:JSON.stringify({
+    email:email.value,
+    password:password.value
+  })
+ }).then(r=>r.json())
+ .then(d=>{
+  localStorage.setItem("token",d.token);
+  location.href="../index.html";
+ });
+}
