c0e45b8250630de407445c59d932dd35238caf34
indian1989 <rahmathussain.hjp@gmail.com>
2026-02-20 04:59:46 +0530

    Update categoryRoutes.js

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,1 +1,27 @@
-categoryRoutes.js
+const express = require("express");
+const router = express.Router();
+
+const categoryController = require("../controllers/categoryController");
+const { protect } = require("../middleware/authMiddleware");
+const { authorize } = require("../middleware/roleMiddleware");
+
+/*
+====================================
+PUBLIC
+====================================
+*/
+
+router.get("/", categoryController.getAllCategories);
+router.get("/:slug", categoryController.getCategoryBySlug);
+
+/*
+====================================
+ADMIN ONLY
+====================================
+*/
+
+router.post("/", protect, authorize("admin"), categoryController.createCategory);
+router.put("/:id", protect, authorize("admin"), categoryController.updateCategory);
+router.delete("/:id", protect, authorize("admin"), categoryController.deleteCategory);
+
+module.exports = router;
