43a47ff0817a4528b4b47a1f63bfc42f676714b6
indian1989 <rahmathussain.hjp@gmail.com>
2026-02-19 18:56:54 +0530

    Create constants.js

---
+++ backend/config/constants.js
@@ -0,0 +1,23 @@
+module.exports = {
+  ROLES: {
+    USER: "user",
+    PROVIDER: "provider",
+    ADMIN: "admin"
+  },
+
+  PLANS: {
+    FREE: "free",
+    PREMIUM: "premium"
+  },
+
+  BUSINESS_STATUS: {
+    PENDING: "pending",
+    APPROVED: "approved",
+    REJECTED: "rejected"
+  },
+
+  PAYMENT_TYPE: {
+    FEATURED: "featured",
+    BOOST: "boost"
+  }
+};
