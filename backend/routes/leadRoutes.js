a58c09755ec74322bed30512563669148a34200d
indian1989 <rahmathussain.hjp@gmail.com>
2026-02-20 05:02:38 +0530

    Update leadRoutes.js

--- backend/routes/leadRoutes.js
+++ backend/routes/leadRoutes.js
@@ -1,1 +1,33 @@
-leadRoutes.js
+const express = require("express");
+const router = express.Router();
+
+const leadController = require("../controllers/leadController");
+const { protect } = require("../middleware/authMiddleware");
+const { authorize } = require("../middleware/roleMiddleware");
+
+/*
+====================================
+PUBLIC (Lead Submit)
+====================================
+*/
+
+router.post("/", leadController.createLead);
+
+/*
+====================================
+PROVIDER
+====================================
+*/
+
+router.get("/my", protect, authorize("provider"), leadController.getProviderLeads);
+
+/*
+====================================
+ADMIN
+====================================
+*/
+
+router.get("/", protect, authorize("admin"), leadController.getAllLeads);
+router.delete("/:id", protect, authorize("admin"), leadController.deleteLead);
+
+module.exports = router;
