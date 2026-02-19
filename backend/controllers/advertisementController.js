fcc7da7a612b3c09e5a192f560b8a49120f2de55
indian1989 <rahmathussain.hjp@gmail.com>
2026-02-19 22:47:10 +0530

    Update advertisementController.js

--- backend/controllers/advertisementController.js
+++ backend/controllers/advertisementController.js
@@ -1,1 +1,46 @@
+const Advertisement = require("../models/Advertisement");
+
+/*
+====================================
+CREATE AD
+====================================
+*/
+
+exports.createAd = async (req, res) => {
+  try {
+    const ad = await Advertisement.create(req.body);
+
+    res.status(201).json({
+      success: true,
+      data: ad
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+/*
+====================================
+GET ACTIVE ADS
+====================================
+*/
+
+exports.getActiveAds = async (req, res) => {
+  try {
+    const ads = await Advertisement.find({
+      isActive: true,
+      expiresAt: { $gte: new Date() }
+    });
+
+    res.status(200).json({
+      success: true,
+      data: ads
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
