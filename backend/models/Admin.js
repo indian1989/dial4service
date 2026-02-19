4b5fe14c22b3c49dcacd77d84329ce52cb43ce6a
indian1989 <rahmathussain.hjp@gmail.com>
2026-02-19 19:19:55 +0530

    Update Admin.js

--- backend/models/Admin.js
+++ backend/models/Admin.js
@@ -1,9 +1,31 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { ROLES } = require("../config/constants");
 
-module.exports = mongoose.model("Admin",
-  new mongoose.Schema({
-    email: String,
-    password: String,
-    role: { type: String, default: "admin" }
-  })
-);
+const adminSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  role: { type: String, default: ROLES.ADMIN }
+}, { timestamps: true });
+
+adminSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+adminSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+adminSchema.methods.generateToken = function () {
+  return jwt.sign(
+    { id: this._id, role: this.role },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_EXPIRE }
+  );
+};
+
+module.exports = mongoose.model("Admin", adminSchema);
