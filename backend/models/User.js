5603c8cbfe82d742129936b2c2307551818574e4
indian1989 <rahmathussain.hjp@gmail.com>
2026-02-19 19:16:50 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,10 +1,58 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { ROLES } = require("../config/constants");
 
-module.exports = mongoose.model("User",
-  new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String,
-    role: { type: String, default: "user" }
-  })
-);
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  phone: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  role: {
+    type: String,
+    default: ROLES.USER
+  },
+  favorites: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Business"
+    }
+  ]
+}, { timestamps: true });
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.methods.generateToken = function () {
+  return jwt.sign(
+    { id: this._id, role: this.role },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_EXPIRE }
+  );
+};
+
+module.exports = mongoose.model("User", userSchema);
