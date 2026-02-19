1af942518da43ed151b2361394f71ff489e89600
indian1989 <rahmathussain.hjp@gmail.com>
2026-02-20 05:01:41 +0530

    Update reviewRoutes.js

--- backend/routes/reviewRoutes.js
+++ backend/routes/reviewRoutes.js
@@ -1,1 +1,26 @@
-reviewRoutes.js
+const express = require("express");
+const router = express.Router();
+
+const reviewController = require("../controllers/reviewController");
+const { protect } = require("../middleware/authMiddleware");
+const { authorize } = require("../middleware/roleMiddleware");
+
+/*
+====================================
+PUBLIC
+====================================
+*/
+
+router.get("/business/:businessId", reviewController.getBusinessReviews);
+
+/*
+====================================
+USER
+====================================
+*/
+
+router.post("/", protect, authorize("user"), reviewController.createReview);
+router.put("/:id", protect, authorize("user"), reviewController.updateReview);
+router.delete("/:id", protect, authorize("user", "admin"), reviewController.deleteReview);
+
+module.exports = router;
