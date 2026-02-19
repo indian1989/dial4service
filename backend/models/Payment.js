90cc90c232229ca36710b87f53b94d53ce930634
indian1989 <rahmathussain.hjp@gmail.com>
2026-02-19 19:30:42 +0530

    Update Payment.js

--- backend/models/Payment.js
+++ backend/models/Payment.js
@@ -1,1 +1,11 @@
-Payment.js
+const mongoose = require("mongoose");
+
+const paymentSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId },
+  type: String, // featured / boost
+  amount: Number,
+  status: { type: String, default: "pending" },
+  transactionId: String
+}, { timestamps: true });
+
+module.exports = mongoose.model("Payment", paymentSchema);
