57bc972ee9d88d160736bd2a9a7b1b121c3f4eba
indian1989 <rahmathussain.hjp@gmail.com>
2026-02-19 22:46:22 +0530

    Update adminController.js

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,1 +1,34 @@
+const Business = require("../models/Business");
+const User = require("../models/User");
+const Provider = require("../models/Provider");
+
+/*
+====================================
+ADMIN DASHBOARD STATS
+====================================
+*/
+
+exports.getAdminDashboard = async (req, res) => {
+  try {
+    const totalUsers = await User.countDocuments();
+    const totalProviders = await Provider.countDocuments();
+    const totalBusinesses = await Business.countDocuments();
+    const pendingBusinesses = await Business.countDocuments({
+      status: "pending"
+    });
+
+    res.status(200).json({
+      success: true,
+      data: {
+        totalUsers,
+        totalProviders,
+        totalBusinesses,
+        pendingBusinesses
+      }
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
