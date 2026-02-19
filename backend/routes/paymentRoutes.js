59a1a3c3cc9d89dd9834ed47f63a2cc6d79d48c9
indian1989 <rahmathussain.hjp@gmail.com>
2026-02-20 05:09:54 +0530

    Update paymentRoutes.js

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -1,1 +1,13 @@
-paymentRoutes.js
+const express = require("express");
+const router = express.Router();
+
+const paymentController = require("../controllers/paymentController");
+const { protect } = require("../middleware/authMiddleware");
+const { authorize } = require("../middleware/roleMiddleware");
+
+router.post("/create-order", protect, authorize("provider"), paymentController.createOrder);
+router.post("/verify", protect, authorize("provider"), paymentController.verifyPayment);
+
+router.get("/", protect, authorize("admin"), paymentController.getAllPayments);
+
+module.exports = router;
