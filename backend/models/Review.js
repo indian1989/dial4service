929d9cef4adad4c22467fbda3313f7a2fe727e54
indian1989 <rahmathussain.hjp@gmail.com>
2026-02-19 19:27:59 +0530

    Update Review.js

--- backend/models/Review.js
+++ backend/models/Review.js
@@ -1,1 +1,10 @@
-Review.js
+const mongoose = require("mongoose");
+
+const reviewSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  business: { type: mongoose.Schema.Types.ObjectId, ref: "Business" },
+  rating: { type: Number, min: 1, max: 5 },
+  comment: String
+}, { timestamps: true });
+
+module.exports = mongoose.model("Review", reviewSchema);
