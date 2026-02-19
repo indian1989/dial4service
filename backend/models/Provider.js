df10de33e96c84aace956dc3c12abe14d715b457
indian1989 <rahmathussain.hjp@gmail.com>
2026-02-19 19:18:46 +0530

    Update Provider.js

--- backend/models/Provider.js
+++ backend/models/Provider.js
@@ -1,10 +1,35 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { ROLES } = require("../config/constants");
 
-module.exports = mongoose.model("Provider",
-  new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String,
-    role: { type: String, default: "provider" }
-  })
-);
+const providerSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  businessName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  phone: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, default: ROLES.PROVIDER },
+  isVerified: { type: Boolean, default: false },
+  plan: { type: String, default: "free" }
+}, { timestamps: true });
+
+providerSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+providerSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+providerSchema.methods.generateToken = function () {
+  return jwt.sign(
+    { id: this._id, role: this.role },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_EXPIRE }
+  );
+};
+
+module.exports = mongoose.model("Provider", providerSchema);
