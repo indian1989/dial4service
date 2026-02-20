8f720eb0c08081d221301efbcf6a354fcb77a8d5
indian1989 <rahmathussain.hjp@gmail.com>
2026-02-20 06:42:47 +0530

    Update and rename seo.js to responseHandler.js

---
+++ backend/utils/responseHandler.js
@@ -0,0 +1,19 @@
+const successResponse = (res, message, data = null) => {
+  return res.status(200).json({
+    success: true,
+    message,
+    data
+  });
+};
+
+const errorResponse = (res, message, statusCode = 400) => {
+  return res.status(statusCode).json({
+    success: false,
+    message
+  });
+};
+
+module.exports = {
+  successResponse,
+  errorResponse
+};
