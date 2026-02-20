87023bf04bd43c7649d32e6ad922898f7ea8ca4b
indian1989 <rahmathussain.hjp@gmail.com>
2026-02-20 07:18:30 +0530

    Update errorMiddleware.js

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,14 +1,32 @@
-exports.notFound = (req, res, next) => {
-  const error = new Error(`Not Found - ${req.originalUrl}`);
-  res.status(404);
-  next(error);
-};
+const errorHandler = (err, req, res, next) => {
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  let message = err.message;
 
-exports.errorHandler = (err, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  // Mongoose Bad ObjectId
+  if (err.name === "CastError") {
+    message = "Resource not found";
+    statusCode = 404;
+  }
+
+  // Mongoose Duplicate Key
+  if (err.code === 11000) {
+    message = "Duplicate field value entered";
+    statusCode = 400;
+  }
+
+  // Mongoose Validation Error
+  if (err.name === "ValidationError") {
+    message = Object.values(err.errors)
+      .map(val => val.message)
+      .join(", ");
+    statusCode = 400;
+  }
 
   res.status(statusCode).json({
-    message: err.message,
+    success: false,
+    message,
     stack: process.env.NODE_ENV === "production" ? null : err.stack
   });
 };
+
+module.exports = errorHandler;
