9e08dc3a018c8b07bde6a31b8255482232de52cb
indian1989 <rahmathussain.hjp@gmail.com>
2026-02-20 06:37:28 +0530

    Update and rename slug.js to slugify.js

--- backend/utils/slug.js
+++
@@ -1,8 +0,0 @@
-const slugify = require("slugify");
-
-function makeUniqueSlug(title) {
-  // basic slug
-  return slugify(title, { lower: true, strict: true });
-}
-
-module.exports = { makeUniqueSlug };
