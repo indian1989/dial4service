8df7e1d0ab28bed9d4de83bcb8f4b0fd59e41e7e
indian1989 <rahmathussain.hjp@gmail.com>
2026-02-21 11:32:02 -0500

    Update adminRoutes.js

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const jwt = require{"jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const { body, validationResult } = require("express-validator");
 
 //Dummy middlewares
