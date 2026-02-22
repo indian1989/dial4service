b7f4ef42545d4536c526e78c90d125eccf8e2d59
indian1989 <rahmathussain.hjp@gmail.com>
2026-02-22 05:42:53 -0500

    Update adminRoutes.js

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -52,33 +52,28 @@ router.post(
   authorize("super-admin"),
   async (req, res) => {
     try {
-      const { name, email, password } = req.body;
+      const { name, email, password, phone } = req.body;
 
       const existing = await User.findOne({ email });
       if (existing) {
         return res.status(400).json({ message: "User already exists" });
       }
 
-      const hash = await bcrypt.hash(password, 10);
-
-      const newAdmin = new User({
+      await User.create({
         name,
         email,
-        password: hash,
+        phone,
+        password,
         role: "admin"
       });
-
-      await newAdmin.save();
 
       res.status(201).json({
         message: "Admin Created Successfully"
       });
 
     } catch (err) {
-      console.error(err);
       res.status(500).json({ message: "Server error" });
     }
   }
 );
-
 module.exports = router;
