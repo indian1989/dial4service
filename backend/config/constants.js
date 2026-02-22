25b28cde6120209bedfb1b560dd58aeadd983f45
indian1989 <rahmathussain.hjp@gmail.com>
2026-02-22 11:15:23 -0500

    Update constants.js

--- backend/config/constants.js
+++ backend/config/constants.js
@@ -4,21 +4,5 @@ module.exports = {
     PROVIDER: "provider",
     ADMIN: "admin",
     SUPER_ADMIN: "super-admin"
-  },
-
-  PLANS: {
-    FREE: "free",
-    PREMIUM: "premium"
-  },
-
-  BUSINESS_STATUS: {
-    PENDING: "pending",
-    APPROVED: "approved",
-    REJECTED: "rejected"
-  },
-
-  PAYMENT_TYPE: {
-    FEATURED: "featured",
-    BOOST: "boost"
   }
 };
