a72bcba09eac902177e3563195507bd3f82d4bc1
indian1989 <rahmathussain.hjp@gmail.com>
2026-02-23 15:14:25 -0500

    Delete frontend/js directory

--- frontend/js/main.js
+++
@@ -1,9 +0,0 @@
-fetch(`${API_BASE_URL}/api/business`)
-.then(res=>res.json())
-.then(data=>{
- let html="";
- data.forEach(b=>{
-  html+=`<p>${b.title} - ${b.city}</p>`;
- });
- document.getElementById("list").innerHTML=html;
-});
